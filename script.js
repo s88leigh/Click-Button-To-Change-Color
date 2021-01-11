b8cee5abb13f0aa0d69f710f5b9d42cc3650844b
Sophie <sophie88leigh@gmail.com>
2021-01-10 20:12:41 -0500

    created html page with pink, blue and reset buttons, empty bboxes and title banner

---
+++ script.js
@@ -0,0 +1,11 @@
+var div = document.getElementById("box");
+function pink() {
+    div.setAttribute("id","pink");
+}
+function blue() {
+    div.setAttribute("id","blue");
+}
+
+function Reset() {
+    div.setAttribute("id", "box");
+}

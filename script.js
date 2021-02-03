769655fe030af34f31065666c242bec3c4d15a65
Sophie <sophie88leigh@gmail.com>
2021-02-02 23:19:54 -0500

    changed demo text, added functions to buttons

--- script.js
+++ script.js
@@ -9,3 +9,14 @@ function blue() {
 function reset() {
     div.setAttribute("id", "white");
 }
+
+
+function green() {
+     document.getElementById("demo").style.color="green";
+}
+function red() {
+    document.getElementById("demo").style.color="red";
+}
+function black() {
+    document.getElementById("demo").style.color="black";
+}

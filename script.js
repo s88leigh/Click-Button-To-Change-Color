0a56cd6732389dd15d28834c3d5c7899c1ff9c4d
Sophie <sophie88leigh@gmail.com>
2021-02-08 19:33:51 -0500

    added function to change background color of square

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//Demo 1
 var div = document.getElementById("box");
 function pink() {
     div.setAttribute("id","pink");
@@ -10,7 +11,7 @@ function reset() {
     div.setAttribute("id", "white");
 }
 
-
+//Demo 2
 function green() {
      document.getElementById("demo").style.color="green";
 }
@@ -19,4 +20,28 @@ function red() {
 }
 function black() {
     document.getElementById("demo").style.color="black";
+}
+
+//Demo 3: If, else statement
+function Pressed() {
+    var text = document.getElementById("Inp1").value;
+
+    if (text==="red"){
+        document.getElementById("square").style.backgroundColor="red";
+    }
+    else if (text==="blue"){
+        document.getElementById("square").style.backgroundColor="blue";
+    }
+    else if (text==="green"){
+        document.getElementById("square").style.backgroundColor="green";
+    }
+    else if (text==="yellow"){
+        document.getElementById("square").style.backgroundColor="yellow";
+    }
+    else if (text==="purple"){
+        document.getElementById("square").style.backgroundColor="purple";
+    }
+    else if (text==="orange"){
+        document.getElementById("square").style.backgroundColor="orange";
+    }
 }

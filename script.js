b06b8933a67646b79e1848e1fc06bcbdfe886135
Sophie <sophie88leigh@gmail.com>
2021-02-12 16:25:10 -0500

    added html, style, script.js for Demo 5

--- script.js
+++ script.js
@@ -78,3 +78,17 @@ function clicked() {
         
     }
 }
+// Demo 5: If, Else If, Else Statement
+function submit() { 
+    var text = parseInt(document.getElementById("inp").value);
+    var output = document.getElementById("output");
+   
+    if (text > 21) {
+        output.innerHTML = "You are over 21. Please drink responsibly!";
+    } else if (text == 21) {
+        output.innerHTML = "You are 21. Please drink responsibly!"
+    }
+    else{
+        output.innerHTML = "Sorry, You are NOT of legal drinking age";
+    }
+}
